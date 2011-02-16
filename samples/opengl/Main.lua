347a6e6f6792608252842879760b4b140f2fda3c
htaunay <htaunay@gmail.com>
2011-02-16 10:05:52 -0200

    Just changed loadui path in sample to lower case, so that it would work also in linux.

--- samples/opengl/Main.lua
+++ samples/opengl/Main.lua
@@ -5,7 +5,7 @@ local qt = require "qt"
 qt.setSearchPaths( "coral", co.getPaths() )
 
 -- Creates all the necessary stuff
-local mainWindow = qt.loadUi "coral:/openGL/MainWindow.ui"
+local mainWindow = qt.loadUi "coral:/opengl/MainWindow.ui"
 local glWidget   = co.new "qt.GLWidget"
 local cubeSample = co.new "opengl.BasicCubeSample"
 
