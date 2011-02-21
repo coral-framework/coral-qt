8e0abd176015a9109d81486b933270d4a1794dc6
pablocael <pablo.cael@gmail.com>
2011-02-21 17:06:03 -0300

    Changed eventHandlerTests to be compatible with the refactored event handling system for Objects

--- tests/eventHandlerTests.lua
+++ tests/eventHandlerTests.lua
@@ -6,7 +6,7 @@ local testWidget = qt.new( "QWidget" )
 
 function testEventNotification()
 	local hit = false
-	testWidget:listen( qt.Event.Close, function() hit = true end )
+	testWidget.onClose = function() hit = true end
 	testWidget.visible = true
 	testWidget:invoke( "close()" )
 	ASSERT_TRUE( hit, "The event was not called" )
