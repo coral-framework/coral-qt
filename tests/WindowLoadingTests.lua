a3ab28b07c72c343d73eef08818e136fa8428737
Pablo Carneiro Elias <pablo.cael@gmail.com>
2011-11-18 13:53:49 -0200

    changed tests to support coral-testkit changes

--- tests/WindowLoadingTests.lua
+++ tests/WindowLoadingTests.lua
@@ -10,11 +10,3 @@ function aWindowShouldBeInvisibleWhenLoaded()
 	local widget = qt.loadUi( file )
 	env.ASSERT_TRUE( not widget.visible, "widget visible when opened" )
 end
-
-function shouldThrowAnExceptionWithAnInexistantFile()
-	env.EXPECT_EXCEPTION( "could not open", qt.loadUi, "inexistant.ui" )
-end
-
-function shouldThrowAnExceptionWithAnInvalidFile()
-	env.EXPECT_EXCEPTION( "error loading ui file", qt.loadUi, "coral:../tests/resources/Invalid.ui" )
-end
